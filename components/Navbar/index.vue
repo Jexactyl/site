b0e9e3b6ed9c791360ca261063e042ea45f3eff8
cmrxnn <cmrxn2005@gmail.com>
2022-07-17 02:04:46 +0100

    Add navbar links

--- components/Navbar/index.vue
+++ components/Navbar/index.vue
@@ -7,9 +7,9 @@
           <p class="font-bold text-xl ml-1 text-pink-300">Jexactyl</p>
       </div>
       <nav class="flex space-x-5 md:space-x-10">
-        <a href="#" class="text-sm text-gray-200 hover:text-pink-300">Documentation</a>
-        <a href="#" class="text-sm text-gray-200 hover:text-pink-300">GitHub</a>
-        <a href="#" class="text-sm text-gray-200 hover:text-pink-300">Discord</a>
+        <a href="https://docs.jexactyl.com" class="text-sm text-gray-200 hover:text-pink-300">Documentation</a>
+        <a href="https://github.com/jexactyl" class="text-sm text-gray-200 hover:text-pink-300">GitHub</a>
+        <a href="https://discord.gg/qttGR4Z5Pk" class="text-sm text-gray-200 hover:text-pink-300">Discord</a>
       </nav>
       <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
         <Button title="Get Started" to="https://docs.jexactyl.com" />
